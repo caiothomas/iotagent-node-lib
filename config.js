65548cab959b36baf3b7f12608f45deb984894be
Caio Thomas <caiioothomas@gmail.com>
2017-02-27 22:43:37 -0300

    teste

--- config.js
+++ config.js
@@ -64,8 +64,8 @@ var config = {
             ]
         }
     },
-    service: 'smartGondor',
-    subservice: '/gardens',
+    service: 'figuardian',
+    subservice: '/figuardian',
     providerUrl: 'http://192.168.56.1:4041',
     deviceRegistrationDuration: 'P1M',
     defaultType: 'Thing'
